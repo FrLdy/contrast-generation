dd4287d4f9fd640fde61f15308c35730fa719526
francois_ledoyen <ledoyenfrancois@gmail.com>
2021-05-25 14:34:31 +0200

    Create Bridge of U-net

---
+++ models/res_u_net/bridge.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+from .std_blocks import ConvBlock
+
+class Bridge(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.bridge = nn.Sequential(
+            ConvBlock(in_channels, out_channels),
+            ConvBlock(out_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.bridge(x)
