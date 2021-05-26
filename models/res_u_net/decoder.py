2e4323b1010d0ce712e97e6ced56fdb5905638d8
francois_ledoyen <ledoyenfrancois@gmail.com>
2021-05-26 13:49:00 +0200

    Fix : Up convolution for resnet input layer

--- models/res_u_net/decoder.py
+++ models/res_u_net/decoder.py
@@ -6,17 +6,25 @@ class UnetDecoder(nn.Module):
     def __init__(self, 
         decoder_channels,
         upsampling_method,
-        out_channels=1
+        out_channels=3
     ):
         super().__init__()
         self.decoder_channels = decoder_channels
         self.upsampling_method = upsampling_method
         self.up_blocks = self.build_up_blocks()
+        self.last_up = UpBlock(
+                *[decoder_channels[-1][1]]*2,
+                decoder_channels[-1][1]+3,
+                decoder_channels[-1][1],
+                upsampling_method="bilinear"
+        )
         self.last_layer = nn.Conv2d(
             in_channels=decoder_channels[-1][1], 
             out_channels=out_channels,
             kernel_size=1, stride=1
         )
+        
+        
 
     def build_up_blocks(self):
         up_blocks = {}
@@ -32,4 +40,6 @@ class UnetDecoder(nn.Module):
     def forward(self, x, encoder_features_maps):
         for block, fm in zip(self.up_blocks.values(), encoder_features_maps) :
             x = block(x, fm)
-        return self.last_layer(x)
+        x = self.last_up(x, encoder_features_maps[-1])
+        x = self.last_layer(x)
+        return x
