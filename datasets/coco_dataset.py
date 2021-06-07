55d1e68a6e6af33dd5acb2a2bf457abbbcc2ab74
francois_ledoyen <ledoyenfrancois@gmail.com>
2021-06-07 14:07:56 +0200

    Rename CocoDataset to CocoPairsDataset

--- datasets/coco_dataset.py
+++ datasets/coco_dataset.py
@@ -5,7 +5,7 @@ import json
 import skimage.io as io
 from pycocotools.coco import COCO
 
-class CocoDataset(data.Dataset):
+class CocoPairsDataset(data.Dataset):
     def __init__(self, coco, paires, transform=None):
         super().__init__()
         self.coco = coco
@@ -20,16 +20,23 @@ class CocoDataset(data.Dataset):
         raw = self.paires[index]
         extract_img = lambda i : io.imread(self.coco.loadImgs(raw[f"img_{i}"]["id"])[0]["coco_url"])
         if self.transform is not None:
-            imgs = (self.transform(extract_img(i)) for i in range(1, 3))
+            imgs = [self.transform(extract_img(i)) for i in range(1, 3)]
         else : 
-            imgs = (extract_img(i) for i in range(1, 3))
-        return imgs
+            imgs = [extract_img(i) for i in range(1, 3)]
+        return imgs + [raw["super_class"]]
 
-def coco_dataset_builder(anns_file, paires_file):
-    return CocoDataset(
+def coco_pairs_dataset(anns_file, paires_file):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224, 224)),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ]) 
+
+    return CocoPairsDataset(
         coco=COCO(anns_file),
         paires=json.load(open(paires_file, "r")),
-        transform=transforms.Compose([
-            transforms.ToTensor()
-        ])
+        transform=transform
     )
