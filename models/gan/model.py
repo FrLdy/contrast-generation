f8ca42d8358a8e7a7279e5cd404f6e4b30e943d1
francois_ledoyen <ledoyenfrancois@gmail.com>
2021-06-16 17:43:22 +0200

    Create Discriminator and Generator nets

---
+++ models/gan/model.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, n_input, n_fm, n_output):
+        super().__init__()
+
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(n_input, n_fm * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(n_fm * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(n_fm * 8, n_fm * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_fm * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( n_fm * 4, n_fm * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_fm * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( n_fm * 2, n_fm, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_fm),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(n_fm, n_output, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, n_input, n_fm):
+        super().__init__()
+
+        self.main = nn.Sequential(
+            nn.Conv2d(n_input, n_fm, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(n_fm, n_fm * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_fm * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(n_fm * 2, n_fm * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_fm * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(n_fm * 4, n_fm * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(n_fm * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(n_fm * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
