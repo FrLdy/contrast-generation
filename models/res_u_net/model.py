af3780c7e6fbc89889e3a5b2462cef7e23198eb3
francois_ledoyen <ledoyenfrancois@gmail.com>
2021-06-25 11:14:18 +0200

    Revert "Add tqdm progress bar to training loops + minor"
    This reverts commit b5f425670a079c1d505a46a2df17892ba5d53631.

--- models/res_u_net/model.py
+++ models/res_u_net/model.py
@@ -5,7 +5,7 @@ from .bridge import Bridge
 from .decoder import UnetDecoder
 
 class ResUnetAutoencoder(nn.Module):
-    def __init__(self, resnet, bridge_out_channels, upsampling_method, output_size, copy_n_crop):
+    def __init__(self, resnet, bridge_out_channels, upsampling_method, output_size):
         super().__init__()
 
         self.bridge_out_channels = bridge_out_channels
@@ -20,8 +20,7 @@ class ResUnetAutoencoder(nn.Module):
         self.decoder = UnetDecoder(
             self.decoder_channels,
             upsampling_method,
-            output_size=output_size,
-            copy_n_crop=copy_n_crop
+            output_size=output_size
         )
 
     def forward(self, x):
