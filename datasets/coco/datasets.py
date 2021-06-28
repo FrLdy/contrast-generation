102057a8b30bf34bae94022d1150ebaca9b5bbb3
francois_ledoyen <ledoyenfrancois@gmail.com>
2021-06-28 15:33:08 +0200

    update dataloading

--- datasets/coco/datasets.py
+++ datasets/coco/datasets.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.transforms as transforms
 import torch.utils.data as data
+import numpy as np
 import json
 import skimage.io as io
 from pycocotools.coco import COCO
@@ -24,7 +25,7 @@ class CocoPairsDataset(data.Dataset):
             imgs = [self.transform(extract_img(i)) for i in range(1, 3)]
         else : 
             imgs = [extract_img(i) for i in range(1, 3)]
-        return imgs + [raw["super_class"]]
+        return np.array(imgs), raw["super_class"]
 
 
 class CocoDataset(data.Dataset):
@@ -40,7 +41,7 @@ class CocoDataset(data.Dataset):
     def __getitem__(self, index):
         data = self.image_ids[index]
         
-        img = io.imread(self.coco.loadImgs(data["id"])[0]["coco_url"])
+        img = np.array(io.imread(self.coco.loadImgs(data["id"])[0]["coco_url"]))
         super_class = data["meta_class"]
 
         if img.shape[0] == 1:
@@ -55,52 +56,65 @@ class CocoDataset(data.Dataset):
 
 
 
-def coco_pairs_dataset(anns, pairs_file, transform):
+def coco_pairs_dataset(anns, pairs_file, transform, ):
     return CocoPairsDataset(
         coco=anns if isinstance(anns, COCO) else COCO(anns),
-        pairs=json.load(open(pairs_file, "r")),
+        pairs=json.load(open(pairs_file, "r"))[slice],
         transform=transform
     )
 
-def coco_singles_dataset(anns, ids_file, transform):
+def coco_singles_dataset(anns, ids_file, transform, slice=slice(None, None)):
     return CocoDataset(
         coco=anns if isinstance(anns, COCO) else COCO(anns),
-        image_ids=json.load(open(ids_file, "r")),
+        image_ids=json.load(open(ids_file, "r"))[slice],
         transform=transform
     )
 
 
-def data_loading():
-    data_dir_pattern = "/Volumes/F_LEDOYEN/ms_coco/annotations/{}.json"
-    data_files = {
-        "coco_anns_all" : "instances_all2017",
-        
-        "all" : {
-            "singles" : "imgs",
-            "pairs" : "pairs"
-        },
-        
-        "sport" : {
-            "singles" : "imgs_sport",
-            "pairs" : "pairs_sport"
-        }  
-    }
+class CocoDataLoader():
+    coco = None
+    def __init__(self) -> None:
+        self.data_dir_pattern = "/Volumes/F_LEDOYEN/ms_coco/annotations/{}.json"
+        self.data_files = {
+            "coco_anns_all" : "instances_all2017",
+            
+            "all" : {
+                "singles" : "imgs",
+                "pairs" : "pairs"
+            },
+            
+            "sport" : {
+                "singles" : "imgs_sport",
+                "pairs" : "pairs_sport"
+            }  
+        }
+        self.data_files = f.map_nested_dicts(self.data_files, lambda k, v : self.data_dir_pattern.format(v))
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize((128, 128)),
-        transforms.Lambda(lambda x : x.repeat(3, 1, 1) if x.shape[0] == 1 else x),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((128, 128)),
+            transforms.Lambda(lambda x : x.repeat(3, 1, 1) if x.shape[0] == 1 else x),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+        ])
+
+
+        if CocoDataLoader.coco is None :
+            CocoDataLoader.coco = COCO(self.data_files["coco_anns_all"]) 
+        self.coco = CocoDataLoader.coco 
+
+    def pairs_dataset(self, slice=slice(None, None)):
+        return coco_pairs_dataset(
+            self.coco, 
+            self.data_files["sport"]["pairs"], 
+            self.transform, slice
+        ) 
+
+    def singles_dataset(self, slice=slice(None, None)):
+        return coco_singles_dataset(
+            self.coco, 
+            self.data_files["sport"]["singles"], 
+            self.transform, slice
         )
-    ])
-
-    data_files = f.map_nested_dicts(data_files, lambda k, v : data_dir_pattern.format(v))
-
-    coco = COCO(data_files["coco_anns_all"]) 
-
-    pairs_dataset = coco_pairs_dataset(coco, data_files["sport"]["pairs"], transform)
-    singles_dataset = coco_singles_dataset(coco, data_files["sport"]["singles"], transform)
-
-    return pairs_dataset, singles_dataset
